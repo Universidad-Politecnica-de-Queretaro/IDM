2ec1aa10057f67241e54010d028e507f43945d38
FranciscoSinecio <136523290+FranciscoSinecio@users.noreply.github.com>
2023-07-04 23:48:23 -0600

    PRUEBAS2.py

--- PRUEBAS2.py
+++ PRUEBAS2.py
@@ -1,216 +1,187 @@
-import pygame
-import random
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget, QMessageBox
+from PyQt5.QtGui import QFont, QColor, QPalette
 
-# Dimensiones de la pantalla
-WIDTH = 800
-HEIGHT = 600
+class VentanaRegistro(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Registro de Usuario")
+        self.setFixedSize(300, 300)
+        
+        self.init_ui()
 
-# Colores
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
+    def init_ui(self):
+        # Crear campos de entrada
+        self.label_nombre = QLabel("Nombre:", self)
+        self.entry_nombre = QLineEdit(self)
 
-# Tamaño de la serpiente, la comida y el obstáculo
-SNAKE_SIZE = 20
-FOOD_SIZE = 20
-OBSTACLE_SIZE = 20
+        self.label_correo = QLabel("Correo electrónico:", self)
+        self.entry_correo = QLineEdit(self)
 
-# Velocidad de movimiento de la serpiente
-SNAKE_SPEED = 20
+        self.label_contraseña = QLabel("Contraseña:", self)
+        self.entry_contraseña = QLineEdit(self)
+        self.entry_contraseña.setEchoMode(QLineEdit.Password)
 
-# Inicializar Pygame
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Juego de la Viborita")
+        # Crear botón de registro
+        self.btn_registrar = QPushButton("Registrar", self)
+        self.btn_registrar.clicked.connect(self.registrar_usuario)
 
-clock = pygame.time.Clock()
+        # Crear layout vertical
+        layout = QVBoxLayout()
+        layout.addWidget(self.label_nombre)
+        layout.addWidget(self.entry_nombre)
+        layout.addWidget(self.label_correo)
+        layout.addWidget(self.entry_correo)
+        layout.addWidget(self.label_contraseña)
+        layout.addWidget(self.entry_contraseña)
+        layout.addWidget(self.btn_registrar)
 
-# Función principal del juego
-def game():
-    # Posición inicial de la serpiente
-    snake_x = WIDTH // 2
-    snake_y = HEIGHT // 2
+        # Crear widget contenedor y establecer el layout
+        widget = QWidget()
+        widget.setLayout(layout)
 
-    # Velocidad inicial de la serpiente
-    snake_dx = SNAKE_SPEED
-    snake_dy = 0
+        # Establecer el widget contenedor como el widget central de la ventana
+        self.setCentralWidget(widget)
 
-    # Lista para almacenar las partes del cuerpo de la serpiente
-    snake_body = []
-    snake_length = 1
+        # Aplicar estilos
+        self.setStyleSheet(
+            "QMainWindow { background-color: #FFFFFF; }"
+            "QLabel { color: #333333; }"
+            "QLineEdit { background-color: #F2F2F2; border: 1px solid #E5E5E5; border-radius: 3px; padding: 4px; }"
+            "QPushButton { background-color: #4CAF50; color: #FFFFFF; border: none; border-radius: 3px; padding: 8px; }"
+            "QPushButton:hover { background-color: #45A049; }"
+        )
 
-    # Posición inicial de la comida
-    food_x = round(random.randrange(0, WIDTH - FOOD_SIZE) / 20.0) * 20.0
-    food_y = round(random.randrange(0, HEIGHT - FOOD_SIZE) / 20.0) * 20.0
+    def registrar_usuario(self):
+        nombre = self.entry_nombre.text()
+        correo = self.entry_correo.text()
+        contraseña = self.entry_contraseña.text()
 
-    # Puntaje y nivel
-    score = 0
-    level = 1
+        # Verificar campos vacíos
+        if not nombre or not correo or not contraseña:
+            QMessageBox.critical(self, "Error de registro", "Error: Todos los campos deben estar llenos.")
+            return
 
-    # Variable para controlar si el juego se está ejecutando
-    game_over = False
+        # Verificar si el usuario ya está registrado
+        if correo in usuarios:
+            QMessageBox.critical(self, "Error de registro", "Error: el correo electrónico ya está registrado.")
+        else:
+            # Registrar nuevo usuario
+            usuarios[correo] = {"nombre": nombre, "contraseña": contraseña}
+            QMessageBox.information(self, "Registro exitoso", "Usuario registrado correctamente.")
+            self.limpiar_campos()
 
-    # Crear obstáculos para niveles avanzados
-    obstacles = []
-    if level > 1:
-        for _ in range(level + 3):
-            obstacle_x = round(random.randrange(0, WIDTH - OBSTACLE_SIZE) / 20.0) * 20.0
-            obstacle_y = round(random.randrange(0, HEIGHT - OBSTACLE_SIZE) / 20.0) * 20.0
-            obstacles.append([obstacle_x, obstacle_y])
+    def limpiar_campos(self):
+        self.entry_nombre.clear()
+        self.entry_correo.clear()
+        self.entry_contraseña.clear()
 
-    # Ciclo principal del juego
-    while not game_over:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+class VentanaInicioSesion(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Inicio de Sesión")
+        self.setFixedSize(300, 200)
+        
+        self.init_ui()
 
-            # Capturar eventos de teclado para mover la serpiente
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT and snake_dx != SNAKE_SPEED:
-                    snake_dx = -SNAKE_SPEED
-                    snake_dy = 0
-                elif event.key == pygame.K_RIGHT and snake_dx != -SNAKE_SPEED:
-                    snake_dx = SNAKE_SPEED
-                    snake_dy = 0
-                elif event.key == pygame.K_UP and snake_dy != SNAKE_SPEED:
-                    snake_dy = -SNAKE_SPEED
-                    snake_dx = 0
-                elif event.key == pygame.K_DOWN and snake_dy != -SNAKE_SPEED:
-                    snake_dy = SNAKE_SPEED
-                    snake_dx = 0
+    def init_ui(self):
+        # Crear campos de entrada
+        self.label_correo = QLabel("Correo electrónico:", self)
+        self.entry_correo = QLineEdit(self)
 
-        # Mover la serpiente
-        snake_x += snake_dx
-        snake_y += snake_dy
+        self.label_contraseña = QLabel("Contraseña:", self)
+        self.entry_contraseña = QLineEdit(self)
+        self.entry_contraseña.setEchoMode(QLineEdit.Password)
 
-        # Comprobar si la serpiente ha colisionado con los bordes de la pantalla
-        if snake_x < 0 or snake_x >= WIDTH or snake_y < 0 or snake_y >= HEIGHT:
-            game_over = True
+        # Crear botón de inicio de sesión
+        self.btn_iniciar_sesion = QPushButton("Iniciar sesión", self)
+        self.btn_iniciar_sesion.clicked.connect(self.iniciar_sesion)
 
-        # Comprobar si la serpiente ha colisionado consigo misma
-        if [snake_x, snake_y] in snake_body[:-1]:
-            game_over = True
+        # Crear layout vertical
+        layout = QVBoxLayout()
+        layout.addWidget(self.label_correo)
+        layout.addWidget(self.entry_correo)
+        layout.addWidget(self.label_contraseña)
+        layout.addWidget(self.entry_contraseña)
+        layout.addWidget(self.btn_iniciar_sesion)
 
-        # Comprobar si la serpiente ha colisionado con un obstáculo
-        for obstacle in obstacles:
-            if snake_x == obstacle[0] and snake_y == obstacle[1]:
-                game_over = True
+        # Crear widget contenedor y establecer el layout
+        widget = QWidget()
+        widget.setLayout(layout)
 
-        # Comprobar si la serpiente ha comido la comida
-        if snake_x == food_x and snake_y == food_y:
-            # Generar nueva posición para la comida
-            food_x = round(random.randrange(0, WIDTH - FOOD_SIZE) / 20.0) * 20.0
-            food_y = round(random.randrange(0, HEIGHT - FOOD_SIZE) / 20.0) * 20.0
+        # Establecer el widget contenedor como el widget central de la ventana
+        self.setCentralWidget(widget)
 
-            # Aumentar la longitud de la serpiente
-            snake_length += 1
+        # Aplicar estilos
+        self.setStyleSheet(
+            "QMainWindow { background-color: #FFFFFF; }"
+            "QLabel { color: #333333; }"
+            "QLineEdit { background-color: #F2F2F2; border: 1px solid #E5E5E5; border-radius: 3px; padding: 4px; }"
+            "QPushButton { background-color: #4CAF50; color: #FFFFFF; border: none; border-radius: 3px; padding: 8px; }"
+            "QPushButton:hover { background-color: #45A049; }"
+        )
 
-            # Incrementar el puntaje
-            score += 1
+    def iniciar_sesion(self):
+        correo = self.entry_correo.text()
+        contraseña = self.entry_contraseña.text()
 
-            # Comprobar si se alcanzó el puntaje necesario para subir de nivel
-            if score % 10 == 0 and level < 4:
-                level += 1
+        # Verificar campos vacíos
+        if not correo or not contraseña:
+            QMessageBox.critical(self, "Error de inicio de sesión", "Error: Todos los campos deben estar llenos.")
+            return
 
-                # Crear nuevos obstáculos para el nuevo nivel
-                obstacles = []
-                for _ in range(level + 3):
-                    obstacle_x = round(random.randrange(0, WIDTH - OBSTACLE_SIZE) / 20.0) * 20.0
-                    obstacle_y = round(random.randrange(0, HEIGHT - OBSTACLE_SIZE) / 20.0) * 20.0
-                    obstacles.append([obstacle_x, obstacle_y])
+        # Verificar si el usuario existe y la contraseña es correcta
+        if correo in usuarios and usuarios[correo]["contraseña"] == contraseña:
+            QMessageBox.information(self, "Inicio de sesión exitoso", "Inicio de sesión exitoso.")
+            self.limpiar_campos()
+        else:
+            QMessageBox.critical(self, "Error de inicio de sesión", "Error: correo o contraseña incorrectos.")
 
-        # Actualizar la lista de partes del cuerpo de la serpiente
-        snake_body.append([snake_x, snake_y])
+    def limpiar_campos(self):
+        self.entry_correo.clear()
+        self.entry_contraseña.clear()
 
-        if len(snake_body) > snake_length:
-            del snake_body[0]
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
 
-        # Dibujar en la pantalla
-        screen.fill(BLACK)
+    # Base de datos ficticia de usuarios registrados
+    usuarios = {}
 
-        # Dibujar la serpiente
-        for part in snake_body:
-            pygame.draw.rect(screen, GREEN, (part[0], part[1], SNAKE_SIZE, SNAKE_SIZE))
+    ventana_registro = VentanaRegistro()
+    ventana_inicio_sesion = VentanaInicioSesion()
 
-        # Dibujar la comida
-        pygame.draw.rect(screen, RED, (food_x, food_y, FOOD_SIZE, FOOD_SIZE))
+    ventana_principal = QMainWindow()
+    ventana_principal.setWindowTitle("Sistema de Registro y Inicio de Sesión")
+    ventana_principal.setFixedSize(300, 200)
 
-        # Dibujar los obstáculos
-        for obstacle in obstacles:
-            pygame.draw.rect(screen, BLUE, (obstacle[0], obstacle[1], OBSTACLE_SIZE, OBSTACLE_SIZE))
+    # Crear botón de registro
+    btn_registro = QPushButton("Registrar", ventana_principal)
+    btn_registro.setStyleSheet("QPushButton { background-color: #1E88E5; color: #FFFFFF; border: none; border-radius: 3px; padding: 8px; }"
+                               "QPushButton:hover { background-color: #1565C0; }")
+    btn_registro.clicked.connect(ventana_registro.show)
 
-        # Mostrar puntaje y nivel
-        font = pygame.font.Font(None, 36)
-        text_score = font.render("Puntaje: " + str(score), True, WHITE)
-        text_level = font.render("Nivel: " + str(level), True, WHITE)
-        screen.blit(text_score, (10, 10))
-        screen.blit(text_level, (10, 50))
+    # Crear botón de inicio de sesión
+    btn_inicio_sesion = QPushButton("Iniciar Sesión", ventana_principal)
+    btn_inicio_sesion.setStyleSheet("QPushButton { background-color: #1E88E5; color: #FFFFFF; border: none; border-radius: 3px; padding: 8px; }"
+                                    "QPushButton:hover { background-color: #1565C0; }")
+    btn_inicio_sesion.clicked.connect(ventana_inicio_sesion.show)
 
-        # Actualizar la pantalla
-        pygame.display.flip()
+    # Crear botón de salida
+    btn_salir = QPushButton("Salir", ventana_principal)
+    btn_salir.setStyleSheet("QPushButton { background-color: #E53935; color: #FFFFFF; border: none; border-radius: 3px; padding: 8px; }"
+                            "QPushButton:hover { background-color: #C62828; }")
+    btn_salir.clicked.connect(app.quit)
 
-        # Controlar la velocidad de actualización
-        clock.tick(10)
+    # Crear layout vertical
+    layout = QVBoxLayout()
+    layout.addWidget(btn_registro)
+    layout.addWidget(btn_inicio_sesion)
+    layout.addWidget(btn_salir)
 
-    # Mostrar mensaje de fin de juego y opciones
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
+    # Crear widget contenedor y establecer el layout
+    central_widget = QWidget(ventana_principal)
+    central_widget.setLayout(layout)
+    ventana_principal.setCentralWidget(central_widget)
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    return True
-                elif event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    quit()
-
-        # Dibujar en la pantalla
-        screen.fill(BLACK)
-
-        # Mostrar mensaje de fin de juego y opciones
-        font = pygame.font.Font(None, 36)
-        text_game_over = font.render("¡Perdiste! ¿Quieres volver a jugar?", True, WHITE)
-        text_retry = font.render("Presiona ENTER para volver a jugar", True, WHITE)
-        text_quit = font.render("Presiona ESCAPE para salir", True, WHITE)
-        screen.blit(text_game_over, (WIDTH // 2 - text_game_over.get_width() // 2, HEIGHT // 2 - 50))
-        screen.blit(text_retry, (WIDTH // 2 - text_retry.get_width() // 2, HEIGHT // 2))
-        screen.blit(text_quit, (WIDTH // 2 - text_quit.get_width() // 2, HEIGHT // 2 + 50))
-
-        pygame.display.flip()
-
-# Función para mostrar el menú
-def menu():
-    menu_running = True
-
-    while menu_running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    game_running = game()
-                    if not game_running:
-                        menu_running = False
-                elif event.key == pygame.K_ESCAPE:
-                    pygame.quit()
-                    quit()
-
-        # Dibujar en la pantalla
-        screen.fill(BLACK)
-
-        # Mostrar opciones del menú
-        font = pygame.font.Font(None, 36)
-        text = font.render("Presiona ENTER para jugar", True, WHITE)
-        screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2))
-
-        pygame.display.flip()
-
-# Ejecutar el menú
-menu()
+    ventana_principal.show()
+    sys.exit(app.exec_())
